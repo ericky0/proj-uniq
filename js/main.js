d7d2bd975e1b65c2d6ead749c92c5a8e8bbb1a88
Erick Hogarth <xandesteamgames977@gmail.com>
2021-08-08 18:44:32 -0300

    implementando muitas coisas

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 const nav = document.querySelector('nav');
 const toggle = document.querySelectorAll('nav .toggle');
-
+const links = document.querySelectorAll('nav ul li a');
 toggle.forEach(element => {
   element.addEventListener('click', () => {
     nav.classList.toggle('show');
   })
 });
 
-const links = document.querySelectorAll('nav ul li a');
-
 links.forEach(element => {
   element.addEventListener('click', () => {
     nav.classList.remove('show');
   })  
 });
+
+const header = document.querySelector('#navbar');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', ( ) => {
+  if(window.scrollY >= navHeight){
+    header.classList.add('scroll');
+  }else{
+    header.classList.remove('scroll');
+  }
+})
